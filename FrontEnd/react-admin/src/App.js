911818e8d599300d0ecbaaebd00795ad6bed751f
Tozlovanu Sandu <77733069+SanduTozlovanu@users.noreply.github.com>
2023-05-10 21:09:20 +0300

    FrontEnd Template initialization finished.

--- FrontEnd/react-admin/src/App.js
+++ FrontEnd/react-admin/src/App.js
@@ -1,21 +1,51 @@
-import {ColorModeContext, useMode} from "./theme";
-import {CssBaseline, ThemeProvider} from "@mui/material";
-import Topbar from "./scenes/global/TopBar";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 
 function App() {
-  const [theme, colorMode] = useMode()
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const handleToggleCollapse = (collapsed) => {
+    setIsCollapsed(!collapsed)
+  };
+
 
   return (
-      <ColorModeContext.Provider value={colorMode}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <div className="app">
-          <main className="content">
-            <Topbar />
+          <Sidebar isSidebar={isSidebar}  onToggleCollapse={handleToggleCollapse}/>
+          <main className="content" style={{marginLeft: `${isCollapsed ? 80 : 270}px`}}>
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+            </Routes>
           </main>
         </div>
-        </ThemeProvider>
-      </ColorModeContext.Provider>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
